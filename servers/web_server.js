645d4d21cc7bd59cbfda7118735f5adf4b1712db
fuhao715 <fuhao715@gmail.com>
2013-08-30 19:09:15 +0800

    添加返回值

--- servers/web_server.js
+++ servers/web_server.js
@@ -8,7 +8,7 @@
 var http = require('http');
 http.createServer(function(req,res){
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+    res.end('Hello World 符浩 \n');
 
 }).listen(8080);
 
