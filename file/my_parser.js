8d11cd78c0209870f0b90bd7bb67b2b865a21757
fuhao715 <fuhao715@gmail.com>
2013-09-04 23:31:22 +0800

    行尾的分号添加

--- file/my_parser.js
+++ file/my_parser.js
@@ -5,12 +5,13 @@
  * Time: 下午2:35
  * To change this template use File | Settings | File Templates.
  */
-var Parser = require('./parser')
+var Parser = require('./parser');
 var fs = require('fs');
 fs.readFile('../config/example_log.txt',function(err,logData){
     if (err) throw err;
     var text = logData.toString();
     console.log(text);
+    console.log("成功读取文件。");
     var parser = new Parser();
     console.log(parser.parse(text));
 });
