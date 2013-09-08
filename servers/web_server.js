1ca4afff852d183a3471bc6e6fc26b5b456014d9
fuhao715 <fuhao-715@163.com>
2013-09-08 22:20:39 +0800

    格式化文件

--- servers/web_server.js
+++ servers/web_server.js
@@ -58,4 +58,4 @@ var requestFunction = function(req, res){
 server.on('request',requestFunction);
 server.listen(8080, "127.0.0.1");
 
-console.log('Server running at http://127.0.0.1:8080/');
+console.log('Server running at http://127.0.0.1:8080/ ');
