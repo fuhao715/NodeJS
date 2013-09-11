313124364a3e97538442dfd1268e42f21203febc
fuhao715 <fuhao715@gmail.com>
2013-09-11 15:50:46 +0800

    重构依赖注入方式，路由

--- route/requestHandlers.js
+++ route/requestHandlers.js
@@ -5,13 +5,37 @@
  * Time: 下午9:49
  * To change this template use File | Settings | File Templates.
  */
-function start() {
+var querystring = require('querystring');
+var fs = require('fs');
+function start(req,res) {
     console.log("Request handler 'start' was called.");
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var html = fs.readFileSync('./app/login.html');
+    res.end(html);
 }
 
-function upload() {
+function login(req, res) {
+    console.log("Request login.");
+    var info ='';
+    req.addListener('data', function(chunk){
+        info += chunk;
+    })
+        .addListener('end', function(){
+            info = querystring.parse(info);
+            res.setHeader('content-type','text/html; charset=UTF-8');//响应编码，如果是html,写在head中也可以
+
+            if(info.name == '张' && info.pwd =='1'){
+                res.end('login success 成功 ' + info.name);
+            }else{
+                res.end('login failed 失败 ' + info.name);
+            }
+        })
+}
+
+function upload(req,res) {
     console.log("Request handler 'upload' was called.");
 }
 
 exports.start = start;
+exports.login = login;
 exports.upload = upload;
--- route/router.js
+++ route/router.js
@@ -5,38 +5,14 @@
  * Time: 下午8:34
  * To change this template use File | Settings | File Templates.
  */
-var querystring = require('querystring');
-var fs = require('fs');
 var url = require("url");
 var requestHandlers = require('./requestHandlers');
-var firstPage = function(res){
-    console.log("Request firstPage.");
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var html = fs.readFileSync('./app/login.html');
-    res.end(html);
-};
-
-var login = function(req, res) {
-    console.log("Request login.");
-    var info ='';
-    req.addListener('data', function(chunk){
-        info += chunk;
-    })
-        .addListener('end', function(){
-            info = querystring.parse(info);
-            res.setHeader('content-type','text/html; charset=UTF-8');//响应编码，如果是html,写在head中也可以
-
-            if(info.name == '张' && info.pwd =='1'){
-                res.end('login success 成功 ' + info.name);
-            }else{
-                res.end('login failed 失败 ' + info.name);
-            }
-        })
-};
 
 var handle = {};
+handle["/favicon.ico"] = requestHandlers.start;
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
+handle["/login"] = requestHandlers.login;
 handle["/upload"] = requestHandlers.upload;
 
 function router(req,res){
@@ -44,20 +20,9 @@ function router(req,res){
     console.log("Request for " + pathname + " received.");
     console.log("About to route a request for " + pathname);
     if (typeof handle[pathname] === 'function') {
-        handle[pathname]();
+        handle[pathname](req,res);
     } else {
         console.log("No request handler found for " + pathname);
     }
-
-    if(req.url == '/'){
-        return firstPage(res);
-    }
-    if(req.url == '/login'){
-        if (req.method != 'POST'){
-            return;
-        }
-        return login(req, res) ;
-    }
-
 }
 exports.router = router;
